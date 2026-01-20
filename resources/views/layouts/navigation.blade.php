a09a463cae3350694f9db82dce95445aafa24fcd
IzzatHaikal0 <izzathaikal.w@gmail.com>
2026-01-21 01:40:16 +0800

    install tailwind

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,21 @@
+<nav class="bg-white border-b border-gray-200">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="/" class="text-xl font-bold text-gray-800">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <a href="/" class="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium text-gray-900">
+                        Dashboard
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
